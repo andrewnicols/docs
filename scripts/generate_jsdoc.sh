e92f248824bbf1923eb5da6d36f91a4a91ae6c79
Andrew Nicols <andrew@nicols.co.uk>
2022-03-15 15:35:53 +0800

    Fetch Moodle version correctly

--- scripts/generate_jsdoc.sh
+++ scripts/generate_jsdoc.sh
@@ -4,7 +4,9 @@ for version in `ls build/html/`
 do
   moodlebranch=`git show "remotes/origin/${version}":.moodlebranch`
   cd .moodle
-  git checkout "origin/${moodlebranch}"
+  git fetch origin "${moodlebranch}"
+  git checkout "remotes/origin/${moodlebranch}"
+  cat version.php
   npm ci
   npx grunt jsdoc
   cd -
