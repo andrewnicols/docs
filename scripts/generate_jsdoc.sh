5fe292e81eedaceeefb1d410790b97703d8dfae5
Andrew Nicols <andrew@nicols.co.uk>
2022-03-15 15:27:23 +0800

    Ensure that the remote branch is checked out

--- scripts/generate_jsdoc.sh
+++ scripts/generate_jsdoc.sh
@@ -4,7 +4,7 @@ for version in `ls build/html/`
 do
   moodlebranch=`git show "remotes/origin/${version}":.moodlebranch`
   cd .moodle
-  git checkout "$moodlebranch"
+  git checkout "origin/${moodlebranch}"
   npm ci
   npx grunt jsdoc
   cd -
