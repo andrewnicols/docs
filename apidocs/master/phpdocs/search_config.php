3f1514a729fa782e22f0d9b4c85881425c3cfd89
andrewnicols <andrewnicols@users.noreply.github.com>
2022-05-08 03:10:01 +0000

    deploy: 03ec3e12b0e12e3012d7087025e4fdee8c295195

--- apidocs/master/phpdocs/search_config.php
+++ apidocs/master/phpdocs/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun May 1 2022 03:31:16 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.3 ",
+  'logo' => "Generated on Sun May 8 2022 03:08:23 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.9.3 ",
 );
 
 ?>
